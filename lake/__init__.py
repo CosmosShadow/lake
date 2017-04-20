5e81490c5735185b6083b4d1bb8e4466d4e0466c
lichen <lichenarthurml@gmail.com>
2017-04-20 19:02:11 +0800

    dir

--- lake/__init__.py
+++ lake/__init__.py
@@ -1,2 +1,3 @@
 # coding: utf-8
 import network
+import dir
---
+++ lake/dir/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import os
+import errno
+
+
+def mkdir(path):
+	try:
+		os.makedirs(path)
+	# Python >2.5
+	except OSError as exc:  # pragma: no cover
+		if exc.errno == errno.EEXIST and os.path.isdir(path):
+			pass
+		else:
+			raise
+
+
+def check_dir(fname):
+	dname = os.path.dirname(fname)
+	if dname not in set([".", "..", ""]):
+		mkdir(dname)
