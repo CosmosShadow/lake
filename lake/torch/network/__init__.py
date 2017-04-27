0526689397b5488a8a49b5cf41c9f115aed6150c
lichen <lichenarthurml@gmail.com>
2017-04-27 19:02:55 +0800

    torch.network.print_net

--- lake/torch/network/__init__.py
+++ lake/torch/network/__init__.py
@@ -1,3 +1,8 @@
 # coding: utf-8
 from base import *
 from unet import *
+
+def print_net(net):
+	print(net)
+	params_count = sum([param.numel() for param in net.parameters()])
+	print('Total number of parameters: %d' % params_count)
