e2b1b982242fd968b74c383f64965f64a0aba52d
lichen <lichenarthurml@gmail.com>
2017-05-08 20:01:24 +0800

    read csv

--- lake/file/__init__.py
+++ lake/file/__init__.py
@@ -6,8 +6,20 @@ def write(data, path):
 		wirte_csv(data, path)
 
 
+def read(path):
+	if path.endswith('.csv'):
+		read_csv(path)
+
+
 def wirte_csv(data, path):
 	with file(path, 'wb') as csvfile:
 		writer = csv.writer(csvfile)
 		for row in data:
 			writer.writerow(row)
+
+
+def read_csv(path, delimiter=','):
+	with file(path, 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter=delimiter)
+		data = [line for line in reader]
+	return data
