c2241763cf52cb9572261c16d7df7b3b962b212c
lichen <lichenarthurml@gmail.com>
2017-06-27 17:51:05 +0800

    fix bug

--- lake/tf/loss.py
+++ lake/tf/loss.py
@@ -5,4 +5,4 @@ def smooth_l1_loss(y_true, y_pred):
 	delta = 0.5
 	x = tf.abs(y_true - y_pred)
 	xx = tf.where(x < delta, 0.5 * x ** 2, delta * (x - 0.5 * delta))
-	return  tf.sum(xx)
+	return  tf.reduce_sum(xx)
