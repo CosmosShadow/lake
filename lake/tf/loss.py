5387043174df8e0188772a35f0e06a6d39644054
lichen <lichenarthurml@gmail.com>
2017-06-27 18:23:22 +0800

    mean, sum

--- lake/tf/loss.py
+++ lake/tf/loss.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 import tensorflow as tf
 
-def smooth_l1_loss(y_true, y_pred):
+def smooth_l1_loss(y_true, y_pred, use_mean=True):
 	delta = 0.5
 	x = tf.abs(y_true - y_pred)
 	xx = tf.where(x < delta, 0.5 * x ** 2, delta * (x - 0.5 * delta))
-	return  tf.reduce_sum(xx)
+	return tf.reduce_mean(xx) if use_mean else tf.reduce_sum(xx)
