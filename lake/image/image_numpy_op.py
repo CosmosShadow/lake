45212b23dfd63fb02b6f30e6c2a247ad3b1d40bb
lichen <lichenarthurml@gmail.com>
2017-07-05 21:05:58 +0800

    整理image, 添加带背景旋转

---
+++ lake/image/image_numpy_op.py
@@ -0,0 +1,73 @@
+# coding: utf-8
+import lake
+import  os
+import sys
+import numpy as np
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+plt.ioff()
+
+from scipy.misc import *
+# imread
+# imsave
+# imresize
+
+
+def csv_to_image(data, save_path, dpi=200):
+	fig = plt.gcf()
+
+	if isinstance(data, str):
+		data = lake.file.read(data)
+
+	names = data[0]
+	record = data[1:]
+
+	x = [item[0] for item in record]
+	plt.xlabel(names[0])
+	for i in range(1, len(names)):
+		y = [item[i] for item in record]
+		plt.plot(x, y, label=names[i])
+
+	plt.legend(names[1:])
+
+	fig.savefig(save_path, dpi=dpi)
+
+
+def save_hist(data, save_path, bins=None, to_percentage=False, xlabel=None, ylabel=None):
+	"""保存成直方图
+	Args:
+		data : numpy
+		bins: like np.linspace(0, 200, 201)
+	"""
+	if (xlabel is None) or (ylabel is None):
+		xlabel = 'x'
+		ylabel = 'y'
+
+	if bins is None:
+		bins = np.linspace(data.min(), data.max()+1, 100)
+
+	fig = plt.gcf()
+	plt.hist(data, bins=bins, alpha=0.5, label='num', color='red')
+	plt.legend(loc='upper right')
+	plt.xlim(0,len(bins)-1)
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+
+	if to_percentage:
+		to_percentage = lambda y, pos: str(round( ( y / float(len(data)) ) * 100.0, 2)) + '%'
+		plt.gca().yaxis.set_major_formatter(FuncFormatter(to_percentage))
+
+	fig.savefig(save_path, dpi=200)
+	plt.close('all')
+
+
+def rgb2gray(rgb):
+	return 0.2126 * rgb[..., 0] + 0.0722 * rgb[..., 1] + 0.7152 * rgb[..., 2]
+
+
+def rgb2y(rgb):
+	return 0.299 * rgb[..., 0] + 0.587 * rgb[..., 1] + 0.114 * rgb[..., 2]
+
---
+++ lake/image/image_pil_op.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+from PIL import Image
+import lake
+import  os
+import sys
+import numpy as np
+import collections
+
+
+def open_image(path):
+	"""打开图片"""
+	return Image.open(path).convert('RGB')
+
+
+def clear_dir(images_dir):
+	"""清理目录下破损图片"""
+	images_path = lake.dir.loop(images_dir, IMG_EXTENSIONS)
+	count = len(images_path)
+	for i, image_path in enumerate(images_path):
+		sys.stdout.write('\r%d/%d' % (i, count))
+		sys.stdout.flush()
+		try:
+			open_image(image_path)
+		except Exception as e:
+			print image_path
+			print ''
+			os.remove(image_path)
+
+
+def rotate_with_background(img, angel, bg=(255,)*4):
+	"""带背景的旋转
+	Args:
+		img: PIL image
+		angel: rotate angel
+		bg: background
+	Returns:
+		rotated image
+	"""
+	assert isinstance(bg, int) or (isinstance(bg, collections.Iterable) and len(bg) == 4)
+	if isinstance(bg, int):
+		bg = (bg, ) * 4
+	# converted to have an alpha layer
+	im2 = img.convert('RGBA')
+	# rotated image
+	rot = im2.rotate(angel, resample=Image.BICUBIC, expand=True)
+	# a white image same size as rotated image
+	fff = Image.new('RGBA', rot.size, bg)
+	# create a composite image using the alpha layer of rot as a mask
+	out = Image.composite(rot, fff, rot)
+	# converting back to mode
+	return out.convert(img.mode)
