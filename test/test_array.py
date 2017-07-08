4dc25fb8f99b1889d33be23996ff9f8ad97a3637
lichen <lichenarthurml@gmail.com>
2017-07-08 22:45:45 +0800

    unit test

--- test/test_array.py
+++ test/test_array.py
@@ -1,23 +1,10 @@
 # coding: utf-8
+from nose.tools import *
 import numpy as np
 import lake
 
 
 class TestArray(object):
-	@classmethod
-	def setup_class(klass):
-		pass
- 
-	@classmethod
-	def teardown_class(klass):
-		pass
- 
-	def setup(self):
-		pass
- 
-	def teardown(self):
-		pass
- 
 	def test_sample(self):
 		count = 100
 		percent = 0.2
@@ -25,45 +12,45 @@ class TestArray(object):
 		data = range(count)
 		sampled, left = lake.array.sample(data, percent)
 
-		assert len(sampled) == int(count * percent)
-		assert len(left) == int(count * (1-percent))
+		assert_equal(len(sampled), int(count * percent))
+		assert_equal(len(left), int(count * (1-percent)))
 
 	def test_exend(self):
 		a = [1, 2, 3]
-		assert len(lake.array.extend(a, 2)) == 2
-		assert len(lake.array.extend(a, 4)) == 4
-		assert len(lake.array.extend(a, 4, -1)) == 4
-		assert lake.array.extend(a, 4, -1)[-1] == -1
-		assert lake.array.extend(a, 4, -1)[0] == 1
+		assert_equal(len(lake.array.extend(a, 2)), 2)
+		assert_equal(len(lake.array.extend(a, 4)), 4)
+		assert_equal(len(lake.array.extend(a, 4, -1)), 4)
+		assert_equal(lake.array.extend(a, 4, -1)[-1], -1)
+		assert_equal(lake.array.extend(a, 4, -1)[0], 1)
 
 	def test_flat(self):
 		a = [[1, 2, 3], [4, 5]]
-		assert len(lake.array.flat(a)) == 5
+		assert_equal(len(lake.array.flat(a)), 5)
 
 	def test_is_in(self):
 		a = [[1, 2], [3, 4], [5, 6]]
 		b = [1, 2]
 		c = [3, 1]
-		assert lake.array.is_in(b, a)
+		assert_true(lake.array.is_in(b, a))
 		assert not lake.array.is_in(c, a)
 
 	def test_split_with_length(self):
 		data = range(100)
 		datas = lake.array.split_with_length(data, 11)
-		assert len(datas) == 100/11 + 1
+		assert_equal(len(datas), 100/11 + 1)
 		datas = lake.array.split_with_length(data, 11, fix_length=True)
-		assert len(datas) == 100/11
+		assert_equal(len(datas), 100/11)
 		datas = lake.array.split_with_length(data, 11, step=6, fix_length=True)
-		assert len(datas) == (100 - 11)/6 + 1
+		assert_equal(len(datas), (100 - 11)/6 + 1)
 		datas = lake.array.split_with_length(data, 11, step=6)
-		assert len(datas) == 100/6 + 1
+		assert_equal(len(datas), 100/6 + 1)
 
 	def test_shuffle(self):
 		a = range(10)
 		b = np.array(a)
 		data = lake.array.Shuffler(a, b)
 		for item in data(2):
-			assert len(item[0]) == 2
+			assert_equal(len(item[0]), 2)
 
 
 
--- test/test_math.py
+++ test/test_math.py
@@ -1,37 +1,13 @@
 # coding: utf-8
+from nose.tools import *
 import lake
 
 
 class TestMathFun(object):
-	@classmethod
-	def setup_class(klass):
-		print """运行测试前，加载数据"""
- 
-	@classmethod
-	def teardown_class(klass):
-		print """运行测试后，释放资源"""
- 
-	def setup(self):
-		print """在每个测试函数运行之前，执行一次"""
- 
-	def teardown(self):
-		print """在每个测试函数运行之后，执行一次"""
- 
 	def test_PieceLinearFun(self):
 		linear = lake.mt.PieceLinearFun(100, 200, 0, 1)
-		assert linear(0) == 0
-		assert linear(100) == 0
-		assert linear(150) == 0.5
-		assert linear(200) == 1
-		assert linear(300) == 1
-
-
-
-
-
-
-
-
-
-
-
+		assert_equal(linear(0), 0)
+		assert_equal(linear(100), 0)
+		assert_equal(linear(150), 0.5)
+		assert_equal(linear(200), 1)
+		assert_equal(linear(300), 1)
