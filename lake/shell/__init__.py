56d6e1289038273673a2e7950983d4db91ca7363
lichen <lichenarthurdata@gmail.com>
2017-09-12 17:00:45 +0800

    完善shell

--- lake/shell/__init__.py
+++ lake/shell/__init__.py
@@ -6,15 +6,21 @@ import os
 import signal
 import subprocess
 import lake
+import time
+
 
 class Shell(object):
-	def __init__(self, cmd, out='print'):
+	def __init__(self, cmd, out='print', sleep=None):
 		super(Shell, self).__init__()
 		self._cmd = cmd
 		self._out = out
+		self._sleep = sleep or 1	#起码等待1s
 		
 	def __enter__(self):
+		print('Shell enter: ', self._cmd)
 		self.pro = subprocess.Popen(self._cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+		time.sleep(self._sleep)
+
 
 	def __exit__(self, exc_type, exc_value, exc_tb):
 		# self.pro.terminate()
@@ -24,6 +30,25 @@ class Shell(object):
 			print(output)
 		elif self._out is not None:
 			lake.file.write(output, self._out)
+		print('Shell exit: ', self._cmd)
+
+
+class MC(object):
+	"""docstring for MC"""
+	def __init__(self, port, size='100m'):
+		super(MC, self).__init__()
+		self._cmd = 'memcached -d -m %s -p %d' % (size, port)
+
+	def __enter__(self):
+		print('memcache start: ', self._cmd)
+		os.system(self._cmd)
+		time.sleep(1)			#等待启动完成
+
+	def __exit__(self, exc_type, exc_value, exc_tb):
+		cmd = '''ps -ef | grep '%s' | awk '{print $2}' | xargs kill -9''' % self._cmd
+		os.system(cmd)
+		print('memcache stop: ', self._cmd)
+
 
 def run(cmd):
 	os.system(cmd)
