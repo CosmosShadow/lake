fa18058d753cf84b0fd53ffd266924814971d1ce
lichen <lichenarthurdata@gmail.com>
2017-09-30 13:42:35 +0800

    增加单元测试j

--- test/test_decorator.py
+++ test/test_decorator.py
@@ -5,10 +5,10 @@ import lake
 
 class TestDecorator(object):
 	def test_register_fun(self):
+		"""动态添加函数到类"""
 		class A(object):
 			def __init__(self):
 				self._name = 'li'
-
 			def fun(self):
 				return self.hello()
 
@@ -17,3 +17,22 @@ class TestDecorator(object):
 			return 'hello, ' + self._name
 
 		assert_equal(A().fun(), 'hello, li')
+
+
+	def test_duplicate_register(self):
+		"""重复注册"""
+		class A(object):
+			def __init__(self):
+				self._name = 'li'
+			def fun(self):
+				return self.hello()
+
+		@lake.decorator.register_fun(A)
+		def hello(self):
+			return 'hello, ' + self._name
+
+		@lake.decorator.register_fun(A)
+		def hello(self):
+			return 'hello again, ' + self._name
+
+		assert_equal(A().fun(), 'hello again, li')
