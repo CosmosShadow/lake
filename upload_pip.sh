1e2c622b04c7c72e9201cbaef2d27c83adffbd35
lichen <lichenarthurdata@gmail.com>
2017-10-05 00:49:40 +0800

    兼容python2与3，刚漏掉了...

--- upload_pip.sh
+++ upload_pip.sh
@@ -1,11 +1,16 @@
 # 打包
 # 参考: https://packaging.python.org/tutorials/distributing-packages/#uploading-your-project-to-pypi
 
+# 清掉文件
+rm -rf build
+rm -rf dist
+rm -rf lake.egg-info
+
 # 切换到python3，以便包通用
 source activate py35
 
-# 本地打包
-python setup.py bdist_wheel
+# 本地打包，--universal适配python2与python3
+python setup.py bdist_wheel --universal
 
 # 上传
 # 上传的账号写在~/.pypirc，形式如下:
