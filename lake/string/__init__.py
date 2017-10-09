f3b0947155b5471b767a614eecbb4a69f3712957
lichen <lichenarthurdata@gmail.com>
2017-10-09 15:37:52 +0800

    查找字符串中最长相同语句

--- lake/string/__init__.py
+++ lake/string/__init__.py
@@ -9,3 +9,24 @@ def humman(value):
 			return "%3.1f%s" % (value, unit)
 		value /= 1024.0
 	return "%.1f%s" % (value, 'Yi')
+
+
+def long_substr(data):
+	"""
+	data数组包含的所有字符串中，查找最长相似项
+	"""
+	substr = ''
+	if len(data) > 1 and len(data[0]) > 0:
+		for i in range(len(data[0])):
+			for j in range(len(data[0])-i+1):
+				if j > len(substr) and is_substr(data[0][i:i+j], data):
+					substr = data[0][i:i+j]
+	return substr
+
+def is_substr(find, data):
+	if len(data) < 1 and len(find) < 1:
+		return False
+	for i in range(len(data)):
+		if find not in data[i]:
+			return False
+	return True
