6dd866060615fdd4b0bfcff7d32e7e231a194341
lichen <lichenarthurml@gmail.com>
2017-11-05 19:05:32 +0800

    添加下载代理

--- lake/network/__init__.py
+++ lake/network/__init__.py
@@ -6,10 +6,10 @@ from . import proxy
 
 import requests
 
-def download_file(url, save_path):
+def download_file(url, save_path, proxies=None):
 	"""下载文件: 主要是大文件，流式下载"""
 	chunk_size = 128
-	r = requests.get(url, stream=True)
+	r = requests.get(url, stream=True, proxies=proxies)
 	with open(save_path, 'wb') as fd:
 		for chunk in r.iter_content(chunk_size):
 			fd.write(chunk)
