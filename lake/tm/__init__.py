e5e49982aa8d0045484743fc12d8ba918adf0416
lichen <lichenarthurml@gmail.com>
2018-01-17 16:57:51 +0800

    feat: 修改时间函数

--- lake/tm/__init__.py
+++ lake/tm/__init__.py
@@ -4,8 +4,28 @@ from __future__ import absolute_import
 from __future__ import print_function
 import time
 
-def current_datetime_str():
-	return time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+def datetime_str(the_time=None):
+	the_time = the_time or time.localtime()
+	return time.strftime("%Y-%m-%d %H-%M-%S", the_time)
 
-def current_date_str():
-	return time.strftime('%Y-%m-%d', time.localtime())
+def date_str(the_time=None):
+	the_time = the_time or time.localtime()
+	return time.strftime('%Y-%m-%d', the_time)
+
+def date_str_simple(the_time=None):
+	the_time = the_time or time.localtime()
+	return time.strftime('%Y%m%d', the_time)
+
+def date(the_time=None):
+	pass
+
+
+if __name__ == '__main__':
+	print(date_str_simple())
+
+	from datetime import datetime
+	print(datetime.now().month)
+	print(datetime(2018, 1, 2))
+	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+	print(type(datetime.now()))
+	print(datetime.now().date())
