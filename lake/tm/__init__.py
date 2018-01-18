81afbc48f8ca8284a845b064471a99fd9c1abee4
lichen <lichenarthurml@gmail.com>
2018-01-18 10:40:23 +0800

    feat: 修改时间函数

--- lake/tm/__init__.py
+++ lake/tm/__init__.py
@@ -2,30 +2,34 @@
 # 时间相关操作
 from __future__ import absolute_import
 from __future__ import print_function
-import time
+import datetime
 
-def datetime_str(the_time=None):
-	the_time = the_time or time.localtime()
-	return time.strftime("%Y-%m-%d %H-%M-%S", the_time)
+datetime_format = '%Y-%m-%d %H:%M:%S'
+date_format = '%Y-%m-%d'
+simple_date_format = '%Y%m%d'
 
-def date_str(the_time=None):
-	the_time = the_time or time.localtime()
-	return time.strftime('%Y-%m-%d', the_time)
 
-def date_str_simple(the_time=None):
-	the_time = the_time or time.localtime()
-	return time.strftime('%Y%m%d', the_time)
+def datetime_str():
+	return datetime.datetime.now().strftime(datetime_format)
 
-def date(the_time=None):
-	pass
+def date_str():
+	return datetime.date.today().strftime(date_format)
+
+def date_str_simple():
+	return datetime.date.today().strftime(simple_date_format)
 
 
 if __name__ == '__main__':
+	print(datetime_str())
+	print(date_str())
 	print(date_str_simple())
 
-	from datetime import datetime
-	print(datetime.now().month)
-	print(datetime(2018, 1, 2))
-	print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-	print(type(datetime.now()))
-	print(datetime.now().date())
+	print(datetime.datetime.now().month)
+	print(datetime.datetime(2018, 1, 2))
+	print(datetime.datetime.now().strftime(datetime_format))
+	print(type(datetime.datetime.now()))
+	print(datetime.datetime.now().date())
+	print(datetime.datetime.now().date().strftime(datetime_format))
+	print(datetime.datetime.now().date().timetuple())
+	print(datetime.datetime.now().date() + datetime.timedelta(days=1))
+	print(type(datetime.date.today()))
