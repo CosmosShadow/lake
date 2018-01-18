4b5117ecf18fde644545b0c1c5c1a7e59cf58285
lichen <lichenarthurml@gmail.com>
2018-01-18 11:40:05 +0800

    feat: 序列化与反序列化

--- lake/file/__init__.py
+++ lake/file/__init__.py
@@ -6,3 +6,16 @@ from .file_op import *
 from .file_read import *
 from .file_write import *
 from .file_pkg import *
+
+
+# 序列化
+def serialize(data, path):
+	import pickle
+	with open(path, 'wb') as f:
+		pickle.dump(data, f)
+
+# 反序列化
+def unserialize(path):
+	import pickle
+	with open(path, 'rb') as f:
+		return pickle.load(f)
