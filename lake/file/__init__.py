e8131f4b6031dabb439610c326bf6a7bccb49edc
lichen <lichenarthurml@gmail.com>
2018-02-26 18:21:08 +0800

    md5

--- lake/file/__init__.py
+++ lake/file/__init__.py
@@ -19,3 +19,26 @@ def unserialize(path):
 	import pickle
 	with open(path, 'rb') as f:
 		return pickle.load(f)
+
+# 文件md5
+def md5(file_path):
+	import hashlib
+	import os
+	f = open(file_path,'rb')  
+	md5_obj = hashlib.md5()
+	while True:
+		d = f.read(8096)
+		if not d:
+			break
+		md5_obj.update(d)
+	hash_code = md5_obj.hexdigest()
+	f.close()
+	md5_str = str(hash_code).lower()
+	return md5_str
+
+
+
+
+
+
+
