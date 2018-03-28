16bd62b855cedcae7fceab9d91e293a7b9ead90d
lichen <lichenarthurml@gmail.com>
2018-03-28 12:04:36 +0800

    feat: 进程中放入到queue的数据必须在join前被回收，不然进程不会结束

--- lake/thread/__init__.py
+++ lake/thread/__init__.py
@@ -10,44 +10,63 @@ def pool_local(locals, target, inputs):
 	Returns:
 		target保存在队列的数据
 	"""
-	data_q = ms.Queue()
-	return_q = ms.Queue()
-
-	for input_data in inputs:
-		data_q.put(input_data)
-
 	thread_count = len(locals)
+
+	data_q = ms.Queue(thread_count)
+	return_q = ms.Queue()
+	
 	ps = []
 	for i in range(thread_count):
 		p = ms.Process(target=target, args=(data_q, return_q, locals[i]))
+		p.start()
 		ps.append(p)
 
-	for p in ps:
-		p.start()
+	print 'all started'
 
-	data_q.close()
-	
-	for p in ps:
-		p.join()
+	for input_data in inputs:
+		data_q.put(input_data)
+	for _ in range(thread_count):
+		data_q.put('stop')
+	print 'put data finish'
 
 	datas = []
-	while not return_q.empty():
-		datas.append(return_q.get())
+	for _ in range(thread_count):
+		print 'index: %d start get' % i
+		datas += return_q.get()
+
+	for i, p in enumerate(ps):
+		print 'index: %d start join' % i
+		p.join()
+
 	return datas
 
 
 if __name__ == '__main__':
 	# 使用方式如下
 	def target(input_q, output_q, local):
-		while not input_q.empty():
-			data = input_q.get()
-			output_q.put(data + local)
-			# time.sleep(0.1)
-
-	locals = [2, 3, 5]
-	inputs = range(100)
-	outputs = pool_local(locals, target, inputs)	
-	print outputs
+		results = []
+		img_path = local
+		m = Match(img_path)
+		while True:
+			try:
+				image_path = input_q.get(timeout=10.0)
+			except Exception as e:
+				print e
+				break
+			if image_path == 'stop':
+				print 'get stop'
+				break
+			try:
+				code, same_count = m.is_contain(image_path)
+			except Exception as e:
+				print image_path
+				print e
+				continue
+			print 'similary: %d   code: %d  image_path: %s' % (same_count, code, image_path)
+			result = (image_path, code, same_count)
+			results.append(result)
+		output_q.put(results)
+		print 'inner terminate'
 
 
 
