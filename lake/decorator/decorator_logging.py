a020bf21cb4725d9055e8eac18eb78054372d11c
lichen <lichenarthurml@gmail.com>
2018-05-02 12:49:31 +0800

    优化logging装饰器，使用内容能够进行分析

--- lake/decorator/decorator_logging.py
+++ lake/decorator/decorator_logging.py
@@ -2,13 +2,22 @@
 # 使用装饰器来对函数进行自动logging
 import json
 
-def logging_fun(log_fun, comment=None):  
+def logging_fun(log_fun, comment=''):
+	"""
+	Args:
+		a : log_fun, eg. logging.error
+		comment: 注释
+	Waning: 函数参数需能够进行json化
+	"""
 	def _deco(func):  
 		def __deco(*args, **kwargs):
-			message = '\n函数名: ' + func.func_name + '\n参数: ' + json.dumps(args) + ' | ' + json.dumps(kwargs) + '\n'
-			if comment is not None:
-				message = '    注释: ' + comment + message
-			log_fun(message)
+			message = {
+				'函数名': func.func_name,
+				'注释': comment,
+				'列表参数': json.dumps(args, ensure_ascii=False),
+				'字典参数': json.dumps(kwargs, ensure_ascii=False)
+			}
+			log_fun(json.dumps(message, ensure_ascii=False))
 			return func(*args, **kwargs)
 		return __deco  
 	return _deco
