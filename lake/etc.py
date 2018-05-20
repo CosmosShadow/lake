ee0880c141d6c8512818fabcc3f9cd04f62e1b1a
lichen <lichenarthurml@gmail.com>
2018-05-20 11:48:00 +0800

    Revert dumps

--- lake/etc.py
+++ lake/etc.py
@@ -11,21 +11,21 @@ def dumps(obj, nested_level=0, output=sys.stdout):
 	"""格式化输出，主要解决utf-8编码无法看的问题"""
 	spacing = '\t'
 	if isinstance(obj, (dict, collections.OrderedDict)):
-		print('%r{' % ((nested_level) * spacing), file=output)
+		print('%s{' % ((nested_level) * spacing), file=output)
 		for k, v in obj.items():
 			if hasattr(v, '__iter__'):
-				print('%r%r:' % ((nested_level + 1) * spacing, k), file=output)
+				print('%s%s:' % ((nested_level + 1) * spacing, k), file=output)
 				dumps(v, nested_level + 1, output)
 			else:
-				print('%r%r: %r' % ((nested_level + 1) * spacing, k, v), file=output)
-		print('%r}' % (nested_level * spacing), file=output)
+				print('%s%s: %s' % ((nested_level + 1) * spacing, k, v), file=output)
+		print('%s}' % (nested_level * spacing), file=output)
 	elif type(obj) == list:
-		print('%r[' % ((nested_level) * spacing), file=output)
+		print('%s[' % ((nested_level) * spacing), file=output)
 		for v in obj:
 			if hasattr(v, '__iter__'):
 				dumps(v, nested_level + 1, output)
 			else:
-				print('%r%r' % ((nested_level + 1) * spacing, v), file=output)
-		print('%r]' % ((nested_level) * spacing), file=output)
+				print('%s%s' % ((nested_level + 1) * spacing, v), file=output)
+		print('%s]' % ((nested_level) * spacing), file=output)
 	else:
-		print('%r%r' % (nested_level * spacing, obj), file=output)
+		print('%s%s' % (nested_level * spacing, obj), file=output)
